208084199c07e3a4ca5b0fc51568d142adde3add
Erik Nomitch <erik@nomitch.com>
2013-07-10 05:02:43 -0700

    completion fix

--- project.zsh
+++ project.zsh
@@ -2,18 +2,18 @@
 # ZSH->PROJECT ===================================
 # ================================================
 export PATH=$PATH:$(dirname $0)/bin
+  
+_project_path=bin/
 
 # Prefer project/
 if [[ -d project/ ]] ; then
   _project_path=project/
-
-# Fallback to bin/
-else
-  _project_path=bin/
 fi
+
 
 _project_cpl() {
   reply=()
+
   for file in `ls $_project_path`
   do
     reply[$(($#reply+1))]=$file
